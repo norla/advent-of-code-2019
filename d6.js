5d466e0cd259cd74426d7e3ed0cd2478247e1ec6
Mattias Norlander <mattias@Mattiass-MBP.lan>
2019-12-07 00:03:04 +0100

    wip

--- d6.js
+++ d6.js
@@ -22,3 +22,23 @@ function count(node) {
 
 const res = nodes.map(count).reduce((acc, n) => acc + n, 0);
 console.log("- - - DEBUG res", JSON.stringify(res, null, 2));
+
+const tree = nodes.reduce((acc, n) => {
+  acc[n] = [input[n]]
+    .concat(Object.entries(input).filter(([_, v]) => v === n).map(([k]) => k))
+    .filter(k => k)
+  return acc;
+}, {});
+
+
+let thePath;
+function findSanta(node, path) {
+  const prev = path[path.length - 1];
+  return tree[node].filter(n => n !== prev).find(n => {
+    if (n === "SAN") thePath = path;
+    return n === "SAN" || findSanta(n, path.concat(node));
+  });
+}
+
+findSanta("YOU", []);
+console.log("FOO", thePath.length - 1); // 409
