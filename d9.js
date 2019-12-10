ba6d84e019b95a19917e57468d149f9f0d404b25
Mattias Norlander (EXT Exp) <mattias.norlander@bnmc02vd2ufhv2q.ad.bonniernews.se>
2019-12-10 15:24:11 +0100

    10 wip

--- d9.js
+++ d9.js
@@ -3,7 +3,7 @@
 const fs = require("fs");
 const events = require("events");
 
-const strInput = fs.readFileSync("./input/d7.txt", "utf-8")
+const strInput = "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99";//fs.readFileSync("./input/d7.txt", "utf-8")
 
 const input = strInput
       .split(",")
