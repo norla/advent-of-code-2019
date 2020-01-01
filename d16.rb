f1eb0c3ca96b4c0b7242975e45b2a556ee8cf49a
Mattias Norlander (EXT Exp) <mattias.norlander@gmail.com>
2020-01-01 23:36:17 +0100

    D16 pt 1

---
+++ d16.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+def getPattern(digit)
+  basePattern = [0,1,0,-1]
+  basePattern.map{ |m| Array.new(digit + 1) { m } }.flatten.rotate
+end
+
+def fft(signal)
+  Array(0..signal.size - 1).map{ |n|
+    pattern = getPattern(n)
+    tot = signal.each_with_index.reduce(0){ |acc, (d, i)|
+      acc + pattern[i % pattern.length] * d
+    }
+    tot.abs % 10
+  }
+end
+
+def run(input, n)
+  digits = String(input).split("").map{|x| x.to_i}
+  n.times do |i|
+    p i
+    digits = fft(digits)
+  end
+  digits
+end
+input = File.read("./input/d16.txt")
+print run(input, 100).join("")
+#print run(input * 10000, 100).join("")
